909ddaf45d002b228d59f25d8015b2ca4416371f
madelei1 <2364119853@qq.com>
2020-03-19 20:41:03 +0800

    lianxi

---
+++ String/Solution_5.java
@@ -0,0 +1,33 @@
+package String;
+
+/**
+ * 求最长回文子串--字符串第五题
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ */
+public class Solution_5 {
+    public boolean isPalindromic(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 暴力解法
+    public String longestPalindrome(String s) {
+        String ans = "";
+        int max = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j <= len; j++) {
+                String test = s.substring(i, j);
+                if (isPalindromic(test) && test.length() > max) {
+                    ans = s.substring(i, j);
+                    max = Math.max(max, ans.length());
+                }
+            }
+        return ans;
+    }
+}
