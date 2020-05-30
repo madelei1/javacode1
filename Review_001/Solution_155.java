640bd9a6c99ee5c2866eb1159f0173973b16f6e8
madelei1 <2364119853@qq.com>
2020-05-30 21:21:11 +0800

    fuxi

---
+++ Review_001/Solution_155.java
@@ -0,0 +1,55 @@
+package Review_001;
+
+import java.util.Stack;
+
+public class Solution_155 {
+
+    /**
+     * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+     *
+     * push(x) —— 将元素 x 推入栈中。
+     * pop() —— 删除栈顶的元素。
+     * top() —— 获取栈顶元素。
+     * getMin() —— 检索栈中的最小元素。
+     *  
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/min-stack
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    Stack<Integer> stack;
+    Stack<Integer> MinStack;
+    public Solution_155() {
+        stack = new Stack<>();
+        MinStack = new Stack<>();
+    }
+
+    public void push(int x) {
+
+        if (stack.isEmpty()) {
+            MinStack.push(x);
+        } else {
+            if (MinStack.peek() >= x) {
+                MinStack.push(x);
+            }
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int x = stack.peek();
+        int y = MinStack.peek();
+        if (x == y) {
+            MinStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return MinStack.peek();
+    }
+}
---
+++ Review_001/Solution_232.java
@@ -0,0 +1,74 @@
+package Review_001;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class Solution_232 {
+    /**
+     * 使用栈实现队列的下列操作：
+     *
+     * push(x) -- 将一个元素放入队列的尾部。
+     * pop() -- 从队列首部移除元素。
+     * peek() -- 返回队列首部的元素。
+     * empty() -- 返回队列是否为空。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/implement-queue-using-stacks
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    /** Initialize your data structure here. */
+    Stack<Integer> stack;
+    Stack<Integer> stack1;
+    public Solution_232() {
+        stack = new Stack<>();
+        stack1 = new Stack<>();
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+
+        stack.push(x);
+
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int x = 0;
+        if (empty()) {
+            throw new RuntimeException("队列为空");
+        }
+        if (stack1.isEmpty()) {
+            while (!stack.isEmpty()) {
+                stack1.push(stack.pop());
+            }
+            x = stack1.pop();
+        } else  {
+            x= stack1.pop();
+        }
+        return x;
+
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        int x = 0;
+        if (empty()) {
+            throw new RuntimeException("队列为空");
+        }
+        if (stack1.isEmpty()) {
+            while (!stack.isEmpty()) {
+                stack1.push(stack.pop());
+            }
+            x = stack1.peek();
+        } else  {
+            x= stack1.peek();
+        }
+        return x;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack.isEmpty() && stack1.isEmpty();
+    }
+}
