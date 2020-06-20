5095a1cc8dd1c8e1dbb952dbad48eada4348f026
madelei1 <2364119853@qq.com>
2020-06-20 21:24:44 +0800

    lianxi

---
+++ JavaWeb/ThreadDemo5.java
@@ -0,0 +1,22 @@
+package JavaWeb;
+
+public class ThreadDemo5 {
+    public static void main(String[] args) {
+
+        Thread t = new Thread("我是一个新线程，起一个比较长度名字没有关系吧。。") {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("hehe");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+
+    }
+}
---
+++ JavaWeb/ThreadDemo6.java
@@ -0,0 +1,44 @@
+package JavaWeb;
+
+public class ThreadDemo6 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("我是一个新线程，我还在运行");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("运行结束");
+            }
+        };
+
+        //先在start之前来调用get方法获取到线程的相关属性
+        //获取线程Id
+        System.out.println(t.getId());//11
+        //获取线程name
+        System.out.println(t.getName());//Thread-0
+        //获取线程状态
+        System.out.println(t.getState());//NEW
+        //获取线程优先级
+        System.out.println(t.getPriority());//5
+        //获取线程是否存活信息
+        System.out.println(t.isAlive());//false
+        //获取线程是否被中断信息
+        System.out.println(t.isInterrupted());//false
+        //判断线程是否为后台线程
+        System.out.println(t.isDaemon());//false
+        t.start();
+        while (t.isAlive()) {
+            System.out.println("该线程还在运行，状态是："+t.getState());
+            Thread.sleep(500);
+        }
+
+
+    }
+}
