dc5e0c87cf68b68d879a5ee5f620bfc4971e4880
madelei1 <2364119853@qq.com>
2020-07-03 20:42:37 +0800

    fuxi

--- Review_001/Map_Set/Solution_138.java
+++ Review_001/Map_Set/Solution_138.java
@@ -1,5 +1,8 @@
 package Review_001.Map_Set;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_138 {
     class Node {
         int val;
@@ -21,6 +24,19 @@ public class Solution_138 {
      */
 
     public Node copyRandomList(Node head) {
-        return head;
+        Map<Node,Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            Node node = new Node(cur.val);
+            map.put(cur,node);
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
     }
 }
