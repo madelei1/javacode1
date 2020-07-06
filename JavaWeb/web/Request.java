f1e1e8d75ae3ed6681db598d0af935d6f819e4a7
madelei1 <2364119853@qq.com>
2020-07-06 22:49:37 +0800

    lianxi

---
+++ JavaWeb/web/Request.java
@@ -0,0 +1,91 @@
+package JavaWeb.web;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+// 描述一个 HTTP 请求
+// 理论上来讲, 请求中也是需要有 body 的. 但是暂时先不管(放到 V3 版本中再说)
+public class Request {
+    private String method;
+    private String url;
+    private String version;
+    private Map<String, String> headers = new HashMap<>();
+    // 加上 URL 中的参数的处理结果.
+    private Map<String, String> parameters = new HashMap<>();
+
+    // 这里的属性不太适合提供 setter 方法. 这里的内容应该都是由对象自动解析的, 而不应该在类外部来修改
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getHeader(String key) {
+        return headers.get(key);
+    }
+
+    public String getParameter(String key) {
+        return parameters.get(key);
+    }
+
+    // 实现构建请求对象的逻辑. 构建过程比较复杂, 使用一个专门的 "工厂方法" 来完成.
+    // 参数来自于 Socket 对象
+    public static Request build(InputStream inputStream) {
+        Request request = new Request();
+        // 实现具体的解析请求的过程, 严格遵守 HTTP 协议
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            // 1. 读取首行
+            String firstLine = bufferedReader.readLine();
+            String[] firstLineToken = firstLine.split(" ");
+            request.method = firstLineToken[0];
+            request.url = firstLineToken[1];
+            request.version = firstLineToken[2];
+            // 2. 解析 URL 中的参数部分
+            //  2.1 先找 ? 在 url 中的位置
+            int pos = request.url.indexOf("?");
+            if (pos != -1) {
+                // 2.2 一个 url 中也是可能不带参数的, 没有 ? 这种就不管了.
+                //     如果有 ? 再来处理参数
+                String parameters = request.url.substring(pos + 1);
+                // 2.3 把整个参数字符串先按照 & 来切分成若干个键值对
+                String[] kvToken = parameters.split("&");
+                for (String kv : kvToken) {
+                    // 针对切分出的键值对的每个部分, 再按照 = 来切分
+                    String[] result = kv.split("=");
+                    request.parameters.put(result[0], result[1]);
+                }
+            }
+            // 3. 处理请求头 header 部分
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null && line.length() != 0) {
+                String[] headerToken = line.split(": ");
+                request.headers.put(headerToken[0], headerToken[1]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return request;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "method='" + method + '\'' +
+                ", url='" + url + '\'' +
+                ", version='" + version + '\'' +
+                ", headers=" + headers +
+                ", parameters=" + parameters +
+                '}';
+    }
+}
---
+++ JavaWeb/web/Response.java
@@ -0,0 +1,60 @@
+package JavaWeb.web;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+// 描述一个 HTTP 响应
+// 并且完成最终的构造过程.
+public class Response {
+    private String version = "HTTP/1.1";
+    private int status;
+    private String message;
+    private Map<String, String> headers = new HashMap<>();
+    // 正文部分可能需要逐步构造. 搞成 StringBuilder 更方便拼接
+    private StringBuilder body = new StringBuilder();
+    // 最终数据需要写入到一个 OutputStream 对象中
+    private OutputStream outputStream = null;
+
+    // 和 Request.build 类似, 这个 build 也是一个工厂方法
+    public static Response build(OutputStream outputStream) {
+        Response response = new Response();
+        response.outputStream = outputStream;
+        return response;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public void writeBody(String content) {
+        body.append(content);
+    }
+
+    public void flush() {
+        // 把一个完整的 Response 数据写入到 OutputStream 中
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream))) {
+            bufferedWriter.write(version + " " + status + " " + message + "\n");
+            // 写 header 之前别忘了有一个特殊的 header 需要构造进去
+            headers.put("Content-Length", body.toString().getBytes().length + "");
+            for (HashMap.Entry<String, String> entry : headers.entrySet()) {
+                bufferedWriter.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+            bufferedWriter.write("\n");
+            bufferedWriter.write(body.toString());
+            // 带缓冲区的流对象, 每次调用 write 都是写到缓冲区中. 调用 flush 操作就是真的把缓冲区
+            // 的数据写入到最终的 Socket 中.
+            bufferedWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
