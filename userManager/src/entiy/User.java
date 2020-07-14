d50aee1c5240724f10824d1a4fa2008a1ed9c240
madelei1 <2364119853@qq.com>
2020-07-14 20:53:01 +0800

    yong hu guan li xi tong

---
+++ userManager/src/entiy/User.java
@@ -0,0 +1,76 @@
+package entiy;
+
+public class User {
+    private String name;
+    private String uesrname;
+    private String password;
+    private String gender;
+    private int age;
+    private String address;
+    private String qq;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUesrname() {
+        return uesrname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUesrname(String uesrname) {
+        this.uesrname = uesrname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
