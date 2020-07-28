96be837740bb395f9d560ba328af4e8171a6b956
madelei1 <2364119853@qq.com>
2020-07-28 21:15:31 +0800

    fuxi

---
+++ Review_002/Solution_136.java
@@ -0,0 +1,26 @@
+package Review_002;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_136 {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=0; i < nums.length; i ++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i],map.get(nums[i])+1);
+            } else {
+                map.put(nums[i],1);
+            }
+
+        }
+        for(int i = 0; i < nums.length; i ++) {
+            if(map.get(nums[i]) == 1) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
+
